8a2a63f08de4e395221c9a1eea9e1781d07e2ccf
welcomemax <welcomemax@icloud.com>
2018-01-29 22:07:11 +0300

    popup opener animation

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -3,22 +3,45 @@ var widgetsData = bgWindow.widgetsData;
 
 var popup = angular.module('popup', []);
 
-popup.controller('popupController', ['$scope', function ($scope) {
+popup.controller('popupController', ['$scope', '$timeout', function ($scope, $timeout) {
+    $scope.popupActive = false;
     $scope.settings = {
         highlight: true
     };
 
     this.$onInit = function () {
+        $scope.togglePopupActive();
+
         $scope.widgetsData = widgetsData;
 
         if ($scope.widgetsData) {
             $scope.count = $scope.widgetsData.length;
         }
     };
+
+    chrome.browserAction.onClicked.addListener(function () {
+        $scope.togglePopupActive();
+    });
+
+    $scope.togglePopupActive = function () {
+        $timeout(function () {
+            $scope.popupActive = !$scope.popupActive;
+        });
+    };
+
+    $scope.highlightWidget = function () {
+        console.log('widget hovered')
+    }
 }]);
 
 popup.directive('widgetsCount', function() {
     return {
         template: '<b>{{count}}</b> widget{{count > 1 ? "s" : ""}} detected'
+    };
+});
+
+popup.directive('widgetsEmpty', function() {
+    return {
+        template: 'Widgets weren\'t detected on the current page'
     };
 });
